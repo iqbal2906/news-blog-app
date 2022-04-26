af985f19e56fb713c00019ad506e180d66364526
Iqbal Sahrizal <iqbal.sahrizal@pazemo.com>
2022-04-26 21:35:14 +0700

    footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Layout.module.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ title, keyword, desciption, children }) {
   return (
@@ -11,8 +12,8 @@ export default function Layout({ title, keyword, desciption, children }) {
         <meta name="keyword" content={keyword} />
       </Head>
       <Header />
-      <div className={styles.container}></div>
-      {children}
+      <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
