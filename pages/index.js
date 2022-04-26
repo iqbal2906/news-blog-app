112e00bdbddf71135524d388077b746f97fc2d17
Iqbal Sahrizal <iqbal.sahrizal@pazemo.com>
2022-04-26 22:13:01 +0700

    custom 404

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,37 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ news }) {
   return (
     <div>
       <Layout>
-        <h1>Home</h1>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+        <h1>Latest News</h1>
+        {news.length === 0 && <h3>No News</h3>}
+        {news.map((item) => (
+          <NewsItem key={item.id} news={item} />
+        ))}
       </Layout>
     </div>
   );
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/news`);
+//   const news = await res.json();
+
+//   return {
+//     props: { news },
+//   };
+// }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/news`);
+  const news = await res.json();
+
+  return {
+    props: { news },
+    revalidate: 1,
+  };
 }
